5d9ab86309275c25632ab2a9de4a621c4f29ec46
Lauren Lunemann <lunemannl@dewv.edu>
2018-09-05 16:54:37 +0000

    Updates via sync script

--- loader.js
+++ loader.js
@@ -2,3 +2,7 @@ import {game} from "./sgc/sgc.js";
 
 game.preloadImage("floor.png");
 game.preloadImage("marcusSheet.png", 48, 48);
+game.preloadImage("fireballSheet.png", 48, 48);
+game.preloadImage("marcusSpellSheet.png", 48, 48);
+game.preloadImage("strangerSheet.png", 48, 48);
+game.preloadImage("strangerSpellSheet.png", 48, 48);
--- wizardsDuel.js
+++ wizardsDuel.js
@@ -26,8 +26,29 @@ class PlayerWizard extends Sprite {
     }
     handleGameLoop() {
         this.y = Math.max(0, this.y);
-        this.y = Math.min(game.displayHeight, this.y);
+        this.y = Math.min(game.displayHeight - this.height, this.y);
+        this.speed = 0;
+    }
+    handleSpacebar() {
+        let spell = new Spell();
+        spell.x = this.x;
+        spell.y = this.y;
+        spell.name = "A spell cast by Marcus";
+        spell.setImage("marcusSpellSheet.png");
+        spell.angle = 0;
     }
 }
 
 let marcus = new PlayerWizard();
+
+class Spell extends Sprite {
+    constructor() {
+        super();
+        this.speed = 200;
+        this.height = 48;
+        this.width = 48;
+        this.defineAnimation("magic", 0, 7);
+        this.playAnimation("magic", true);
+    }
+}
+
