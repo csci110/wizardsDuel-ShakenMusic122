b2d1f15cb1f9a7776365424cab7546b5da1e01a7
Lauren Lunemann <lunemannl@dewv.edu>
2018-09-05 15:58:39 +0000

    Updates via sync script

---
+++ loader.js
@@ -0,0 +1,4 @@
+import {game} from "./sgc/sgc.js";
+
+game.preloadImage("floor.png");
+game.preloadImage("marcusSheet.png", 48, 48);
--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,1 +1,33 @@
+import {game, Sprite} from "./sgc/sgc.js";
+game.setBackground("floor.png");
 
+class PlayerWizard extends Sprite {
+    constructor() {
+        super();
+        this.name = "Marcus the Wizard";
+        this.setImage("marcusSheet.png");
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up", 0, 2);
+        this.height = 48;
+        this.width = 48;
+        this.x = this.width;
+        this.y = this.height;
+        this.speedWhenWalking = 100;
+    }
+    handleDownArrowKey() {
+        this.playAnimation("down");
+        this.speed = this.speedWhenWalking;
+        this.angle = 270;
+    }
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
+    }
+    handleGameLoop() {
+        this.y = Math.max(0, this.y);
+        this.y = Math.min(game.displayHeight, this.y);
+    }
+}
+
+let marcus = new PlayerWizard();
